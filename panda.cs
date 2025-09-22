51a651afa1cdebb88227d1149a44017102365db6
EvaRosales1 <rosalesep1@my.pbsc.edu>
2025-09-21 23:28:17 -0400

    Add files via upload

--- panda.cs
+++ panda.cs
@@ -1,1 +1,21 @@
+using System;
+
+public class Panda
+{
+    public string Name { get; set; }
+
+    public Panda(string name)
+    {
+        Name = name;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Panda myPanda = new Panda("Rachel");
+        Console.WriteLine("The panda's name is " + myPanda.Name);
+    }
+}
 
